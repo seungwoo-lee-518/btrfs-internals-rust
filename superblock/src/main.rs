e0dcf04d1b6afa4a1c923495dab21e2238c5362b
SEUNGWOO LEE <seungwoo.lee.518@proton.me>
2023-10-28 19:16:44 +0900

    feat(superblock/src/main.rs): WIP implementing structs

--- superblock/src/main.rs
+++ superblock/src/main.rs
@@ -1,14 +1,43 @@
-use std::{fs::File, io::{Seek, Read}};
+use std::{fs::File, io::{Seek, Read, Cursor}};
 
-// use binrw::BinRead;
+use binrw::BinRead;
 
-// #[derive(BinRead)]
-// #[allow(dead_code)]
-// struct Superblock {
-// }
+#[derive(BinRead, Debug)]
+#[allow(dead_code)]
+#[br(little)]
+struct Superblock {
+    csum: [u8; 32],             // 32
+    fsid: [u8; 16],             // 16
+    bytenr: u64,                // 8
+    flags: u64,                 // 8
+    magic: [u8; 0x8],           // 8
+    generation: u64,            // 8
+    root_tree: u64,             // 8
+    chunk_tree: u64,            // 8
+    log_tree: u64,              // 8
+    log_root_transid: u64,      // 8
+    total_bytes: u64,           // 8
+    bytes_used: u64,            // 8
+    root_dir_objectid: u64,     // 8
+    num_devices: u64,           // 8
+    sectorsize: u32,            // 4
+    nodesize: u32,              // 4
+    leafsize: u32,              // 4
+    stripesize: u32,            // 4
+    sys_chunk_array_size: u32,  // 4
+    chunk_root_generation: u64, // 8
+    compat_flags: u64,          // 8
+    compat_ro_flags: u64,       // 8
+    incompat_flags: u64,        // 8
+    csum_type: u16,             // 2
+    root_level: u8,             // 1
+    chunk_root_level: u8,       // 1
+    log_root_level: u8,         // 1
+}
 
 /// Superblock Position for BTRFS
 static BTRFS_SUPERBLOCK_POS: u64 = 0x10000;
+static BTRFS_SUPERBLOCK_SIZE: usize = 4096;
 
 fn main() {
     let mut f = match File::open("btrfs.img") {
@@ -19,7 +48,7 @@ fn main() {
         }
     };
 
-    let mut contents: Vec<u8> = Vec::with_capacity(20);
+    let mut contents = Vec::with_capacity(BTRFS_SUPERBLOCK_SIZE);
 
     match f.seek(std::io::SeekFrom::Start(BTRFS_SUPERBLOCK_POS)) {
         Ok(v) => {
@@ -30,11 +59,21 @@ fn main() {
             std::process::exit(1);
         },
     };
-    
-    if let Err(err) = f.read_to_end(&mut contents) {
+
+    if let Err(err) = f.take(BTRFS_SUPERBLOCK_SIZE as u64).read_to_end(&mut contents) {
         eprintln!("got error while read: {err}");
         std::process::exit(1);
     }
 
-    println!("{:?}", contents)
+    println!("raw contents: {:?}, len={:?}", contents, contents.len());
+
+    let superblock = match Superblock::read(&mut Cursor::new(contents)) {
+        Ok(v) => v,
+        Err(err) => {
+            eprintln!("got error while read: {err}");
+            std::process::exit(1)
+        }
+    };
+
+    println!("superblock: {:?}", superblock);
 }
