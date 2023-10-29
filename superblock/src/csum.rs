c89f4eee67ee63e6ad9822df2e011cbe85766f60
SEUNGWOO LEE <seungwoo.lee.518@proton.me>
2023-10-29 16:11:19 +0900

    feat(superblock/src/csum.rs): add crc32 hasher

--- superblock/src/csum.rs
+++ superblock/src/csum.rs
@@ -1,1 +1,14 @@
+use binrw::{io::Cursor, BinWriterExt};
+use anyhow::Result;
+use crate::Superblock;
 
+#[allow(dead_code)]
+/// Get Hash of the superblock
+pub fn get_hash(superblock: Superblock) -> Result<u32> {
+    let b = Vec::new();
+    let mut s = superblock.clone();
+    s.csum = [0u8; 32];
+    let mut writer = Cursor::new(b.clone());
+    writer.write_le(&s)?;
+    Ok(crc32fast::hash(b.as_slice()))
+}
